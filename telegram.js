46a46f833f6c0dbc8317aff56d706035bee07f7d
Kigvuzyy <miwanpivet@gmail.com>
2023-06-26 04:13:12 +0300

    new file:   calendar.css 	modified:   index.html 	modified:   styles.css 	modified:   telegram.js

--- telegram.js
+++ telegram.js
@@ -1,1 +1,8 @@
-console.log(Telegram);
+const client = window.Telegram.WebApp;
+
+const closeWindows = () => {
+    client.close();
+}
+
+const button = document.getElementById("onClose");
+button.addEventListener("click", closeWindows);
