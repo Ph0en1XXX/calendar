8ab97fb4adca1648218695b5d3a74e1d092b53eb
Kigvuzyy <miwanpivet@gmail.com>
2023-06-26 05:35:58 +0300

    modified:   calendar.js

--- calendar.js
+++ calendar.js
@@ -3,7 +3,7 @@
 //Added previous month and next month view
 
 const client = window.Telegram.WebApp;
-console.log(client.initDataUnsafe.user.id);
+console.log(client.initDataUnsafe?.user);
 
 function CalendarControl() {
     const calendar = new Date();
@@ -75,7 +75,7 @@ function CalendarControl() {
       },
       selectDate: function (e) {          
         const payload = {
-          userid: client.initDataUnsafe.user.id || 0,
+          userid: client.initDataUnsafe?.user || 0,
           date: (new Date(`${e.target.textContent} ${
             calendarControl.calMonthName[calendar.getMonth()]
           } ${calendar.getFullYear()}`)).toISOString()
